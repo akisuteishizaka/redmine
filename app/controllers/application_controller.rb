96615cda1a04077b7be8591a88228421624e639f
taiti <taiti@stone-rise.com>
2015-06-13 04:49:00 +0000

    application_controller changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -618,3 +618,16 @@ class ApplicationController < ActionController::Base
     api_request? ? false : super
   end
 end
+
+require 'digest'
+ 
+ class AdminController < ApplicationController
+  before_filter :authenticate
+ 
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      md5_of_password = Digest::MD5.hexdigest(password)
+      username == 'redmine' && md5_of_password == 'e10adc3949ba59abbe56e057f20f883e'
+    end
+  end
+end
